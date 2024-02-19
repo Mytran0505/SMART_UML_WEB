5465d0249a90016514862fb14d39c1682af5d46c
Mytran0505 <mytran0505itlker@gmail.com>
2024-02-19 16:25:09 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SMART_UML_WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
