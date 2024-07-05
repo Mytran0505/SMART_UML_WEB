a91ed48c4a05accbe2a39c8350d995c3429e6676
quyen20520296 <20520296@gm.uit.edu.vn>
2024-07-06 00:41:34 +0700

    update ddl following erd

--- SMART_UML_WEB/Controllers/TextToERDController.cs
+++ SMART_UML_WEB/Controllers/TextToERDController.cs
@@ -6,6 +6,7 @@ using SMART_UML_WEB.Models;
 using Newtonsoft.Json;
 using System.Net.Http;
 using Newtonsoft.Json.Linq;
+using System.Text.RegularExpressions;
 
 namespace SMART_UML_WEB.Controllers
 {
@@ -100,6 +101,79 @@ namespace SMART_UML_WEB.Controllers
             string umlTextInHexString = Convert.ToHexString(bytes);
 
             return Json(umlTextInHexString);
+        }
+
+        public IActionResult ProcessPlantUMLInputTextToDDL(string umlInputText)
+        {
+            DDLSupport _ddlSupport = new DDLSupport();
+            // Split the input into lines
+            string[] lines = umlInputText.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+
+            // Dictionaries to store entities, primary keys, and foreign keys
+            var entities = new Dictionary<string, List<string>>();
+            var primaryKeys = new Dictionary<string, string>();
+            var foreignKeys = new List<Tuple<string, string, string, string>>();
+
+            string currentEntity = null;
+
+            // Process each line of the UML script
+            foreach (var scriptLine in lines)
+            {
+                var entityMatch = Regex.Match(scriptLine, @"entity\s+""(.+)""\s+as\s+(\w+)\s*\{");
+                var attributeMatch = Regex.Match(scriptLine, @"^\s*[\*\+]\s*(\w+)");
+                var relationMatch = Regex.Match(scriptLine, @"(\w+)\s*\|\w*\{\s*(\w+)");
+
+                if (entityMatch.Success)
+                {
+                    currentEntity = entityMatch.Groups[2].Value;
+                    entities[currentEntity] = new List<string>();
+                }
+                else if (attributeMatch.Success && currentEntity != null)
+                {
+                    entities[currentEntity].Add(attributeMatch.Groups[1].Value);
+                    if (attributeMatch.Groups[0].ToString().StartsWith("*"))
+                    {
+                        primaryKeys[currentEntity] = attributeMatch.Groups[1].Value;
+                    }
+                }
+                else if (relationMatch.Success)
+                {
+                    string parentEntity = relationMatch.Groups[1].Value;
+                    string childEntity = relationMatch.Groups[2].Value;
+
+                    if (entities.ContainsKey(parentEntity) && entities.ContainsKey(childEntity))
+                    {
+                        string parentKey = primaryKeys[parentEntity];
+                        foreignKeys.Add(Tuple.Create(childEntity, parentKey, parentEntity, parentKey));
+                    }
+                }
+            }
+
+            // StringBuilder to construct the DDL statements
+            var ddl = new StringBuilder();
+
+            // Generate DDL for each entity
+            foreach (var entity in entities)
+            {
+                ddl.AppendLine($"CREATE TABLE {entity.Key} (");
+                foreach (var attribute in entity.Value)
+                {
+                    var dataType = _ddlSupport.GetSqlDataType(attribute);
+                    ddl.AppendLine($"    {attribute} {dataType},");
+                }
+
+                if (primaryKeys.ContainsKey(entity.Key))
+                {
+                    ddl.AppendLine($"    CONSTRAINT PK_{entity.Key} PRIMARY KEY({primaryKeys[entity.Key]})");
+                }
+
+                ddl.AppendLine(");");
+                ddl.AppendLine();
+            }
+
+            var ddlResult = ddl.ToString();
+
+            return Json(ddlResult);
         }
 
         public IActionResult TextToERDResult()
---
+++ SMART_UML_WEB/Models/DDLSupport.cs
@@ -0,0 +1,35 @@
+﻿namespace SMART_UML_WEB.Models
+{
+    public class DDLSupport
+    {
+        // Dictionary to map common words to SQL data types
+        public Dictionary<string, string> dataTypes = new Dictionary<string, string>
+        {
+            { "mã", "INT" },
+            { "tên", "VARCHAR(100)" },
+            { "text", "VARCHAR(255)" },
+            { "số", "INT" },
+            { "decimal", "DECIMAL(10, 2)" },
+            { "ngày", "DATE" },
+            { "giờ", "TIME" },
+            { "ngày_giờ", "DATETIME" },
+            { "float", "FLOAT" },
+            { "int", "INT" }
+        };
+
+        // Function to determine the SQL data type based on the attribute name
+        public string GetSqlDataType(string word)
+        {
+            word = word.ToLower();
+            if (word.Contains("số"))
+                return dataTypes["số"];
+            if (word.Contains("ngày"))
+                return dataTypes["ngày"];
+            if (word.Contains("mã"))
+                return dataTypes["mã"];
+            if (word.Contains("tên"))
+                return dataTypes["tên"];
+            return dataTypes["text"];
+        }
+    }
+}
